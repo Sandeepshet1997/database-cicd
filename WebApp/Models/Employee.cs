86a710ebf6249e521e57569521f23a371625bdfa
Sandeep M Shet <sshet006@nam.ad.pwcinternal.com>
2020-03-25 22:38:28 +0530

    initial code

---
+++ WebApp/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Fullname { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
